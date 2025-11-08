ab872af9e8677ba0f444703cd394d9a1802c2866
JasonVery <jvery2001@gmail.com>
2025-11-08 11:59:13 -0700

    Basic Init Setup

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#define LED 2
+
+// Only runs once
+void setup()
+{
+  pinMode(LED, OUTPUT);
+}
+
+// Main loop
+void loop()
+{
+  // digitalWrite(LED, HIGH);
+  // delay(500);
+  // digitalWrite(LED, LOW);
+  // delay(500);
+}
